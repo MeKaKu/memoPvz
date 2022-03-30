ad8851382681a47df03c0c0118899fc767ec20b0
diker <OUJIANJAPAN@163.com>
2022-03-30 13:59:03 +0800

    主菜单

---
+++ PVZ/Assets/Scripts/UI/MenuUI/LevelButton.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(Image))]
+public class LevelButton : BaseUI, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, ICanvasRaycastFilter
+{
+    Image image;
+    RectTransform rect;
+    Vector2 originPos;
+    Sprite normalSprite;
+    public Sprite hoverSprite;
+    public float delayTime = 2;
+    bool isClicked;//是否被点击过了
+    private void Awake() {
+        rect = GetComponent<RectTransform>();
+        originPos = rect.anchoredPosition;
+        image = GetComponent<Image>();
+        normalSprite = image.sprite;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        rect.anchoredPosition = originPos - Vector2.up * 5;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        image.sprite = hoverSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        image.sprite = normalSprite;
+        rect.anchoredPosition = originPos;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        rect.anchoredPosition = originPos;
+        isClicked = true;
+        //TODO:按下button事件
+        Invoke("ChangeSprite", .1f);
+        Invoke("OnLevelButtonClicked", delayTime);
+    }
+
+    void ChangeSprite(){
+        if(image.sprite == normalSprite){
+            image.sprite = hoverSprite;
+        }
+        else{
+            image.sprite = normalSprite;
+        }
+        Invoke("ChangeSprite", .1f);
+    }
+
+    public virtual void OnLevelButtonClicked(){
+        CancelInvoke("ChangeSprite");
+        SceneManager.LoadScene(1);
+    }
+
+    public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+    {
+        return !isClicked;
+    }
+}
